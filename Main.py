23c791137b5255114b7226c35d29d2a1605e436b
Kayleigh Callaway <callawayk6700@my.uwstout.edu>
2019-11-06 16:17:57 -0600

    Got the map to work

--- Main.py
+++ Main.py
@@ -6,15 +6,15 @@ import Vertices
 import Edges
 import random
 
-root = Tk()
-canvas = Canvas(root, bg="black", height=1100, width=2000)
-image = PhotoImage(file="us.gif")
-# image = image.zoom(25)
-# image = image.subsample(15)
-# root.attributes("-fullscreen", True)
-label = Label(root, image=image)
-label.place(x=0, y=0)
-canvas.pack()
+# root = Tk()
+# canvas = Canvas(root, bg="black", height=1100, width=2000)
+# image = PhotoImage(file="us.gif")
+# # image = image.zoom(25)
+# # image = image.subsample(15)
+# # root.attributes("-fullscreen", True)
+# label = Label(root, image=image)
+# label.place(x=0, y=0)
+# canvas.pack()
 
 
 
@@ -26,31 +26,36 @@ def main() -> None:
 
     pygame.init()
 
+    pygame.init()
+    screen = pygame.display.set_mode(size=[1200, 625])
+    bg_color = [255, 255, 255]
+    screen.fill(bg_color)
+    pygame.display.set_caption('Disease Graph')
+    image = pygame.image.load('us.gif')
+
     # Game loop
     running = True
     fps = 60
     dt = 1 / fps
     clock = pygame.time.Clock()
-
     while running:
-        root.update()
+        # root.update()
+        screen.fill(bg_color)
+        screen.blit(image, (0, 0))
         # Redraw
         for o in objects:
-            o.draw(canvas)
+            pass
+            # o.draw(canvas)
 
-        # Show what we have drawn
-        clock.tick(fps)
-
-
+        pygame.display.flip()
         # Event Loop
         for e in pygame.event.get():
             if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
                 running = False
             elif e.type == pygame.KEYDOWN:
-                print("damn")
                 if e.key == pygame.K_SPACE:
-                    print("fuck")
-                    root.attributes("-fullscreen", False)
+                    pass
+                    # root.attributes("-fullscreen", False)
 
 
 
