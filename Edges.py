eccb329d819d2ecf6138dee17495a196f7f4f51e
Kayleigh Callaway <callawayk6700@my.uwstout.edu>
2019-11-12 09:23:36 -0600

    Buttons working, working on getting the information of the diseases in.

---
+++ Edges.py
@@ -0,0 +1,6 @@
+
+class Edges:
+    def __init__(self, typeOfTransport, weight):
+        self.type = typeOfTransport
+        self.weight = weight
+
--- Main.py
+++ Main.py
@@ -1,16 +1,33 @@
 from typing import Tuple, List
 
 import pygame
-from tkinter import *
-import Vertices
 import Edges
 import random
 
 
+class Button:
+    def __init__(self, width, height, center, text, background, font, id):
+        self.surface = pygame.Surface((width, height))
+        self.rect = self.surface.get_rect()
+        self.surface.fill(background)
+        self.rect.center = center
+        self.textSurf = font.render(text, 1, [0, 0, 0])
+        self.textRect = self.textSurf.get_rect(center = (width / 2, height / 2))
+        self.surface.blit(self.textSurf, self.textRect)
+        self.rect = pygame.Rect(center, (width, height))
+        self.id = id
+
+    def draw(self, screen):
+        screen.blit(self.surface, self.rect)
+        pygame.draw.rect(screen, [0, 0, 0], self.rect, 1)
+
+
 def main() -> None:
 
-    objects = [
-        # Vertices(pos=(200, 400))
+    edges = [
+        # Edges("animal", random.randint(1, 15)),
+        # Edges("water", random.randint(1, 15)),
+        # Edges("air", random.randint(1, 15))
     ]
 
     pygame.init()
@@ -22,13 +39,21 @@ def main() -> None:
     black = [0, 0, 0]
     pygame.display.set_caption('Disease Graph')
     image = pygame.image.load('us.gif')
+    font = pygame.font.Font("freesansbold.ttf", 16)
+
+    buttons = [
+        Button(80, 40, (900, 200), "Cholera", [0, 0, 150], font, "Cholera"),
+        Button(100, 40, (900, 300), "Spanish Flu", [150, 0, 0], font, "Flu"),
+        Button(80, 40, (900, 400), "Plague", [0, 150, 0], font, "Plague")
+    ]
+
 
     # Game loop
     running = True
     fps = 60
     dt = 1 / fps
     clock = pygame.time.Clock()
-    font = pygame.font.Font("freesansbold.ttf", 16)
+
     while running:
         screen.fill(bg_color)
         screen.blit(image, (0, 0))
@@ -64,8 +89,11 @@ def main() -> None:
         screen.blit(font.render("Denver", True, black), [300, 240])
         pygame.draw.circle(screen, black, (300, 230), 10)
 
+        for b in buttons:
+            b.draw(screen)
+
         # Redraw
-        for o in objects:
+        for o in edges:
             pass
 
         pygame.display.flip()
@@ -73,9 +101,16 @@ def main() -> None:
         for e in pygame.event.get():
             if e.type == pygame.QUIT or e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
                 running = False
-            elif e.type == pygame.KEYDOWN:
-                if e.key == pygame.K_SPACE:
-                    pass
+            elif e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
+                for b in buttons:
+                    if b.rect.collidepoint(pygame.mouse.get_pos()):
+                        if b.id == "Cholera":
+                            print("Cholera clicked")
+                        elif b.id == "Flu":
+                            print("Flu clicked")
+                        elif b.id == "Plague":
+                            print("Plague clicked")
+
 
 
 
